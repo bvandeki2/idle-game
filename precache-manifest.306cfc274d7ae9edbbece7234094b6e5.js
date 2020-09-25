afc8d20b9c34ac0433d80e484df8c5afedfc0763
Brendan Vande Kieft <brendanvk@gmail.com>
2020-09-24 21:12:41 -0700

    Updates

---
+++ precache-manifest.306cfc274d7ae9edbbece7234094b6e5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c655c29890c9b329aa8d1e52626ea3f",
+    "url": "/idle-game/index.html"
+  },
+  {
+    "revision": "36a5a6026de02a1ec0fa",
+    "url": "/idle-game/static/css/main.7cc3b93f.chunk.css"
+  },
+  {
+    "revision": "48548f987bbb20e14872",
+    "url": "/idle-game/static/js/2.9470787c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/idle-game/static/js/2.9470787c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a5a6026de02a1ec0fa",
+    "url": "/idle-game/static/js/main.6f413d06.chunk.js"
+  },
+  {
+    "revision": "6a9109a06b5052bd7dea",
+    "url": "/idle-game/static/js/runtime-main.77ffee8c.js"
+  },
+  {
+    "revision": "3cc723b7f9c4f9918e1d380fc39bd6ae",
+    "url": "/idle-game/static/media/bluecircle.3cc723b7.svg"
+  },
+  {
+    "revision": "7abc0ac40b793edd2b19fd4e9b6b8e87",
+    "url": "/idle-game/static/media/credit.7abc0ac4.svg"
+  },
+  {
+    "revision": "3ea27515a66f611545590f400110348e",
+    "url": "/idle-game/static/media/greencircle.3ea27515.svg"
+  },
+  {
+    "revision": "787d10859f17f3831ba7d216e7418344",
+    "url": "/idle-game/static/media/land.787d1085.svg"
+  },
+  {
+    "revision": "5819b0ee83254d8bf3bcba11bcf9ccc6",
+    "url": "/idle-game/static/media/redcircle.5819b0ee.svg"
+  }
+]);
